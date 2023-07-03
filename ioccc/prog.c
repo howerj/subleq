fc885cf09b5db5cbc97e48821fe532d5cf41cc99
Richard James Howe <howe.r.j.89@gmail.com>
2023-07-03 20:07:28 +0000

    Removed constants from IOCCC candidate
    Removed some constants from the IOCCC and changed the Makefile around a
    bit. Nothing serious here, it still seems to work as well.

--- ioccc/prog.c
+++ ioccc/prog.c
@@ -1,20 +1,20 @@
 #define r(X) return X
 #define l __LINE__/
 #define F if
-typedef long I;
+typedef long I;typedef char Y;
 #define G goto
 #include <stdio.h>
 typedef FILE*i;
 #define f __func__
 enum{h,u=!h,d,D,U=-u,k=l d,K,R=K+d,w,x=u<<K,C=x<<(u+l u)};
-I V(I _){r(_-l(f[h]-'P'));}
+I V(I _){r(_-l(f[h]-w*(w+d)));}
 I X(I _,I O,I o){r((o&_)|V(-(V(-O)|o)));}/*this doesn't do what you think it does*/
 I L(I _){r(X(_,_^_,V(u<<(h[f]/K))));}/*this does*/
 I H(I _,I O){r(O?H(X(X(h,U,_),_,O),X(_,h,O)*d):_);}/*multiply*/
 I A(I _){r(u<<H(_,U));}
 I M(I _){r(_<x?H(u<<_,U):U);}
 /*big oink*/I y(I O,i o){F(fputc(O,o)<h)r(U);r(fflush(o));}
-I S(i ingest,i excrete,I*m/*not feeling well*/,I D,I(*E[static u])(I)){
+I S(i ingest,i excrete,I*m/*not feeling well*/,I D,I(*E[u])(I)){
 	I volatile a,r,s,e,_=h,O;_:
 	a=m[h[E](_)],O=h[E](u+_)[m];
 	r=X(H(H(m[h[E](O)],X(h,U,(h[E](a))[m])),u),h,d[E](D));
@@ -23,19 +23,19 @@ I S(i ingest,i excrete,I*m/*not feeling well*/,I D,I(*E[static u])(I)){
 	_+=l R;
 	!X(X(h,U,a),a,M(D))?
 		*(h[E](O)+m)=X(fgetc(ingest),h,E[d](D)):
-	!X(X(h,-!!V(D),O),O,(f[h]^'Q')[E](D))?
+	!X(X(h,-!!V(D),O),O,(f[h]/(l u)-u)[E](D))?
 		a=y((E-E)[E](a)[m],excrete):(h[E](O)[m]=r,s)?_=e:h;
 	F(a<h)r(K);
 	/*F(-!X(V(-_),_,L(_)))G _;*/F(_<C)G _;
 	r(h);
 }
 #include <stdlib.h>
-int main(int q,char**v){
+int main(int q,Y**v){
 	F(q<d)r(u);
 	I r[C<<u]={h},_=h,j=atoi(v[u]),o=d,s=h;i O;
 	F(j<w||j>x)r(d);O:
-	F(!(O=fopen(v[o++],"r")))r(D);o:
-	F(fscanf(O,"%ld,",&s)<=h)G I;r[L(_++)]=X(s,h,M(j));G o;I:
+	F(!(O=fopen(v[o++],(Y[d]){D+(l u)*D,h})))r(D);o:
+	F(fscanf(O,(Y[]){l u-u,-u+h[f],f[h]-(w+u),w*K+k,h},&s)<=h)G I;r[L(_++)]=X(s,h,M(j));G o;I:
 	F(fclose(O)<h)r(k);
 	F(o-q)G O;
 	r(S(stdin,stdout,r,j,(I(*[])(I)){&L,A,M,&A}));
