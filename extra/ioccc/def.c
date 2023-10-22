f6277cef3c9f91a5bb49224ba93b9225b437a999
Richard James Howe <howe.r.j.89@gmail.com>
2023-10-22 21:48:25 +0000

    12224: Added another IOCCC one-line candidate
    Another IOCCC candidate has been added, another one-liner, in this
    instance the program is defined via the Makefile/command line. The
    subfolder should be cleaned up and the various candidates renamed.

--- extra/ioccc/def.c
+++ extra/ioccc/def.c
@@ -1,5 +1,4 @@
-main(p,y)char**y;{short m[p*9],a,b,A;
-for(;--p;)m[p-1]=atoi(y[p]);
+short m[1<<16]={X},a,b,A,p;main(){
 for(;~p;){
 A=m[a=m[p++]],b=m[p],p+=2;
 ~a?~b?p=(m[b]-=A)>0?p:m[p-1]:putchar(A):(m[b]=getchar());
