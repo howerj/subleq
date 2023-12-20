fd83eea8a82572276437c44a6ce3105ff8aa624b
Philippe Brochard <philippe.brochard@nomadeducation.fr>
2023-12-20 21:58:36 +0100

    polishing

--- boot/subleq-boot.asm
+++ boot/subleq-boot.asm
@@ -1,159 +1,162 @@
 ;;; Source: https://stackoverflow.com/questions/21846342/how-to-use-more-than-512-bytes-of-my-own-bootable-floppy/69622813#69622813
-	bits 16
-	cpu	8086
+    bits 16
+    cpu 8086
 
-	[org 0x7c00]
-								; stack and segment setup
-	xor ax, ax
-	mov es, ax
-	mov ds, ax
-	mov bp, 0x1200              ; thank you user ecm for notifying me to not use
-								; 0x8000 as the stack address
-	mov ss, ax                  ; thank you user ecm for notifying me to add this specified line of code
-	mov sp, bp
-								; load more than 512 bytes into memory
-	mov ah, 0x02                ; read sectors
-	mov al, 65	                ; number of sectors to read
-	mov ch, 0x00                ; cylinder idx
-	mov dh, 0x00                ; head idx
-	mov cl, 0x02                ; sector idx
-	mov dl, 0x00                ; disk idx
-	mov bx, data                ; address of more than 512 bytes
-	int 0x13
+    [org 0x7c00]
+                                ; stack and segment setup
+    xor ax, ax
+    mov es, ax
+    mov ds, ax
+    mov bp, 0x1200              ; thank you user ecm for notifying me to not use
+                                ; 0x8000 as the stack address
+    mov ss, ax                  ; thank you user ecm for notifying me to add this specified line of code
+    mov sp, bp
+                                ; load more than 512 bytes into memory
+    mov ah, 0x02                ; read sectors
+    mov al, 65                  ; number of sectors to read
+    mov ch, 0x00                ; cylinder idx
+    mov dh, 0x00                ; head idx
+    mov cl, 0x02                ; sector idx
+    mov dl, 0x00                ; disk idx
+    mov bx, data                ; address of more than 512 bytes
+    int 0x13
 
-								; because i'm tired you can do the error checking by checking if al is the
-								; same as what you set it to and checking for the carry flag
+                                ; because i'm tired you can do the error checking by checking if al is the
+                                ; same as what you set it to and checking for the carry flag
 
-	call cls
+    call cls
 
-	mov dx, 0x0000
-	call moveto
+    mov dx, 0x0000
+    call moveto
 
-	mov ax, 97
-	call putchar
+    mov ax, 'G'
+    call putchar
+    mov ax, 'o'
+    call putchar
+    mov ax, 13
+    call putchar
 
-	xor si, si                  ; sp = SI
+    ;; 0. if (pc < 0) halt
+    ;; pc += 3
+    ;; 1. A == -1  [B] = getchar
+    ;; 2. B == -1  putchar [A]
+    ;; 3. [B] = [B] - [A]; if ([B] <= 0) pc = C
 
-	;; 0. if (pc < 0) halt
-	;; pc += 3
-	;; 1. A < 0  [B] = getchar
-	;; 2. B < 0  putchar [A]
-	;; 3. [B] = [B] - [A]; if ([B] <= 0) pc = C
+    xor si, si                  ; pc = SI
 
 subleq:
-	cmp si, 0
-	jl  exit
+    cmp si, 0
+    jl  exit
 
-	mov ax,[data + si]         			; A = AX
-	mov bx, word [data + si + 2]     	; B = BX
-	mov cx, word [data + si + 4]     	; C = CX
+    mov ax,[data + si]                  ; A = AX
+    mov bx, word [data + si + 2]        ; B = BX
+    mov cx, word [data + si + 4]        ; C = CX
 
-	add si, 6
+    add si, 6
 
-	cmp ax, -1                   ; 1. A == -1 ?  [B] = getchar
-	je  .A_NEG_GETCHAR
+    cmp ax, -1                   ; 1. A == -1 ?  [B] = getchar
+    je  .A_NEG_GETCHAR
 
-	cmp bx, -1                   ; 2. B == -1 ?  putchar [A]
-	je  .B_NEG_PUTCHAR
+    cmp bx, -1                   ; 2. B == -1 ?  putchar [A]
+    je  .B_NEG_PUTCHAR
 
-	jmp .ELSE                   ; 3. [B] = [B] - [A]; if ([B] <= 0) pc = C
+    jmp .ELSE                   ; 3. [B] = [B] - [A]; if ([B] <= 0) pc = C
 
  .A_NEG_GETCHAR:
-	mov di, bx
-	shl di, 1
-	call getchar
-	mov word [data + di], ax
-	jmp subleq
+    mov di, bx
+    shl di, 1
+    call getchar
+    mov word [data + di], ax
+    jmp subleq
 
  .B_NEG_PUTCHAR:
-	mov di, ax
-	shl di, 1
-	mov ax, word [data + di]
-	call putchar
-	jmp subleq
+    mov di, ax
+    shl di, 1
+    mov ax, word [data + di]
+    call putchar
+    jmp subleq
 
  .ELSE:
-	mov di, ax                   ; 3. [B] = [B] - [A]
-	shl di, 1
-	mov ax, [data + di]
+    mov di, ax                   ; 3. [B] = [B] - [A]
+    shl di, 1
+    mov ax, [data + di]
 
-	mov di, bx
-	shl di, 1
-	mov bx, word [data + di]
+    mov di, bx
+    shl di, 1
+    mov bx, word [data + di]
 
-	sub bx, ax
+    sub bx, ax
 
-	mov word [data + di], bx
+    mov word [data + di], bx
 
-	cmp bx, 0
-	jg subleq					; [B] > 0
+    cmp bx, 0
+    jg subleq                   ; [B] > 0
 
-	shl cx, 1					; [B] <= 0 ! pc = c
-	mov si, cx
-	jmp subleq
+    shl cx, 1                   ; [B] <= 0 ! pc = c
+    mov si, cx
+    jmp subleq
 
 
 ;;;************************************
 ;;;          Subroutines              *
 ;;;************************************
 exit:
-	mov ax, 88
-	call putchar
-
-	cli
-	hlt
+    mov ax, 'X'
+    call putchar
+    cli
+    hlt
 
 cls:
-	mov ah, 0x07                ; tells BIOS to scroll down window
-	mov al, 0x00                ; clear entire window
-	mov bh, 0x07                ; white on black
-	mov cx, 0x00                ; specifies top left of screen as (0,0)
-	mov dh, 0x18                ; 18h = 24 rows of chars
-	mov dl, 0x4f                ; 4fh = 79 cols of chars
-	int 10h                     ; calls video interrupt
-	ret
+    mov ah, 0x07                ; tells BIOS to scroll down window
+    mov al, 0x00                ; clear entire window
+    mov bh, 0x07                ; white on black
+    mov cx, 0x00                ; specifies top left of screen as (0,0)
+    mov dh, 0x18                ; 18h = 24 rows of chars
+    mov dl, 0x4f                ; 4fh = 79 cols of chars
+    int 10h                     ; calls video interrupt
+    ret
 
 moveto:                         ; move cursor to DH = Row, DL = Column
-	mov ah, 0x02
-	mov bh, 0x00
-	int 10h
-	ret
+    mov ah, 0x02
+    mov bh, 0x00
+    int 10h
+    ret
 
 putchar:                        ; put char in al
-	mov ah, 0x0E
-	mov bh, 0x00
-	int 0x10
-	cmp al, 13
-	je  .LF
-	ret
+    mov ah, 0x0E
+    mov bh, 0x00
+    int 0x10
+    cmp al, 13
+    je  .LF
+    ret
  .LF:
-	mov al, 10
-	jmp putchar
+    mov al, 10
+    jmp putchar
 
 getchar:                        ; get char in al
-	mov ah, 0x00
-	int 0x16
-	xor ah, ah
+    mov ah, 0x00
+    int 0x16
+    xor ah, ah
 
-	;; Uncomment for an FR keyboard (azerty)
-	;; %include "./keyb-FR.asm"
+    ;; Uncomment for an FR keyboard (azerty)
+    %include "./keyb-FR.asm"
 
-	push ax						; echo char
-	push bx
-	call putchar
-	pop bx
-	pop ax
-	ret
+    push ax                     ; echo char
+    push bx
+    call putchar
+    pop bx
+    pop ax
+    ret
 
-	times 510-($-$$) db 0
-	dw 0xAA55
+    times 510-($-$$) db 0
+    dw 0xAA55
 
-	;; more than 512 bytes program
+    ;; more than 512 bytes program
 data:
-	;;%include "./simple-dec.asm"
-	;;%include "./test-3rd-dec.asm"
-	;;%include "./hello-world-dec.asm"
-	%include "./eforth-dec.asm"
+    ;;%include "./simple-dec.asm"
+    ;;%include "./test-3rd-dec.asm"
+    ;;%include "./hello-world-dec.asm"
+    %include "./eforth-dec.asm"
 
-	;; amount of zeros = 512 + (number of sectors read * 512)
-	times 32768-($-$$) db 0
+    ;; amount of zeros = 512 + (number of sectors read * 512)
+    times 32768-($-$$) db 0
